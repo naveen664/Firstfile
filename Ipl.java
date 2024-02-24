2e45ded4c63f43cad317092b24f3691d6780585c
naveen664 <naveenredn@gmail.com>
2024-02-24 16:13:06 +0530

    forth commit

--- Ipl.java
+++ Ipl.java
@@ -2,5 +2,7 @@ class Ipl {
     public static void main(String[] args) {
         System.out.println("hi all!"); 
         System.out.println("first commit");
+        System.out.println("third commit");
+        System.out.println("forth commit");
     }
 }
